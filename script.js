dbb25027e3a12700d24f6f93c6d5917fe1e93905
rncraven94 <neilcraven2013@gmail.com>
2024-01-17 19:51:14 -0500

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 use: "strict";
+
+const submitBtn = document.getElementById("integerInput");
+
+submitBtn.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log("success");
+});
